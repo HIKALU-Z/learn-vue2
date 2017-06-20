b0d4e122c63ea59239fb0ef96ef41635d3ae7ef4
HIKALU <play3a@126.com>
2017-06-21 01:41:49 +0800

    learn to use componets with in componets

--- main.js
+++ main.js
@@ -3,11 +3,29 @@
  */
 
 
+Vue.component('task-list',{
+    template:`
+    <div>
+      <task v-for="task in tasks">{{task.task}}</task>
+    </div>
+    `,
+
+    data(){
+        return{
+            tasks:[
+                {task:'123',id:1},
+                {task:'123',id:2},
+                {task:'123',id:3},
+                {task:'123',id:4}
+            ]
+        }
+    }
+});
 
 Vue.component('task',{
     template:'<li><slot></slot></li>'
 });
 
 new Vue({
-    el:'#root'
+    el:'#root',
 });
