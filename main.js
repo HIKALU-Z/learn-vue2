b3685bea604d44d92da9df58c730961c9623789f
HIKALU <play3a@126.com>
2017-06-22 20:40:10 +0800

    learn to combine bulmaCss and componet

--- main.js
+++ main.js
@@ -3,29 +3,49 @@
  */
 
 
-Vue.component('task-list',{
-    template:`
+Vue.component('message', {
+    props: ['title', 'body'],
+    data(){
+        return{
+            isVisible: true
+        }   
+    },
+    template: `
+        <article class="message" v-show="isVisible">
+            <div class="message-header">
+                {{title}}
+                <button class="delete" @click="isVisible =false"></button>
+            </div>
+            <div class="message-body">
+                {{body}}
+            </div>
+        </article>
+    `
+});
+
+Vue.component('task-list', {
+    template: `
     <div>
       <task v-for="task in tasks">{{task.task}}</task>
     </div>
     `,
 
-    data(){
-        return{
-            tasks:[
-                {task:'123',id:1},
-                {task:'123',id:2},
-                {task:'123',id:3},
-                {task:'123',id:4}
+    data() {
+        return {
+            tasks: [
+                { task: '123', id: 1 },
+                { task: '123', id: 2 },
+                { task: '123', id: 3 },
+                { task: '123', id: 4 }
             ]
         }
     }
 });
 
-Vue.component('task',{
-    template:'<li><slot></slot></li>'
+Vue.component('task', {
+    template: '<li><slot></slot></li>'
 });
 
 new Vue({
-    el:'#root',
+    el: '#root',
 });
