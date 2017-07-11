75a1d2a3456b0c6d7ea0d2052d6df9f8a2f29789
HIKALU <play3a@126.com>
2017-07-11 21:50:42 +0800

    learn to use slot Named Slots in a Nutshell

--- main.js
+++ main.js
@@ -25,6 +25,36 @@ Vue.component('coupon',{
     }
 });
 
+Vue.component('model',{
+    template:`
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">
+                    <slot name="header"></slot>
+                </p>
+                <button class="delete"></button>
+            </header>
+            <section class="modal-card-body">
+                <slot>default content here</slot>
+            </section>
+            <footer class="modal-card-foot">
+                <slot name="footer">
+                    <a class="button is-success">Save</a>
+                    <a class="button">Cancel</a>
+                </slot> 
+            </footer>
+        </div>
+    </div>
+    `,
+     methods:{
+        onCouponApplied(){
+            Event.fire('applied')
+        }
+    }
+});
+
 new Vue({
     el: '#root',
     data:{
